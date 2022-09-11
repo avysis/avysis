d714e4a39d79852464dbf80f8256f29e33878126
0xC4aE1e5 <111705094+0xC4aE1e5@users.noreply.github.com>
2022-09-11 16:43:05 -0700

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 cd %~dp0
-powershell ps2exe island.ps1 -noConsole -iconFile islandsmall.ico
-powershell ps2exe islandbg.ps1 -noConsole -iconFile islandsmall.ico
+powershell Import-Module ps2exe; ps2exe island.ps1 -noConsole -iconFile islandsmall.ico
+powershell Import-Module ps2exe; ps2exe islandbg.ps1 -noConsole -iconFile islandsmall.ico
 iscc setup.iss
