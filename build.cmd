9f531bb9c1596014df5608e1f0cafa56631ee8e7
0xC4aE1e5 <111705094+0xC4aE1e5@users.noreply.github.com>
2022-09-13 17:07:04 -0600

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-cd %~dp0
+cd /d %~dp0
 powershell Import-Module ps2exe; ps2exe avysis.ps1 -noConsole -iconFile avysissmall.ico
 powershell Import-Module ps2exe; ps2exe avysisbg.ps1 -noConsole -iconFile avysissmall.ico
 iscc setup.iss
